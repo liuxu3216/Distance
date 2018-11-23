2ceb1c42d21e6f4819d4488b8dc02c82c49d3942
kaolv3216 <458137665@qq.com>
2018-11-23 16:48:24 +0800

    初始化

---
+++ distance/src/main/java/com/exercise/distance/DistanceApplication.java
@@ -0,0 +1,12 @@
+package com.exercise.distance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistanceApplication.class, args);
+    }
+}
